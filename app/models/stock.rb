3153c2b3091397d61fc7a3c71592cb46a81fbbaa
abdoukercode <almath1106>
2016-12-12 22:15:01 +0100

    user and stock association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,15 @@
 class Stock < ApplicationRecord
 
-    def self.find_by_ticker(symbol)
-        where(ticker: symbol).first
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    def self.find_by_ticker(tiker_symbol)
+        where(ticker: tiker_symbol).first
         
     end
 
-    def self.new_from_lookup(symbol)
-        looked_up_stock = StockQuote::Stock.quote(symbol)
+    def self.new_from_lookup(tiker_symbol)
+        looked_up_stock = StockQuote::Stock.quote(tiker_symbol)
         return nil unless looked_up_stock.name
 
         new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
